7eea46bfbe146ceb0f38854562f4a6dce9c74629
shivam dwivedi <shivamdwivedi618@gmail.com>
2021-10-30 20:52:02 +0530

    accout holder   name  in account settings

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate{
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      // return true//just for testing ng serve
+      //return true//just for testing ng serve
       this.user = JSON.parse(sessionStorage.getItem('psmUser')) || {};
       // console.log("auth guard",this.user)
       if(this.user?.password){
