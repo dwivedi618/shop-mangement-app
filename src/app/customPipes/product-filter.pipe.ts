b557028712c2106ec6a0bf6916e1580499488b66
satyam <satyam.dwivedi@polestarllp.com>
2021-12-29 18:59:51 +0530

    filter logic implemented

--- src/app/customPipes/product-filter.pipe.ts
+++ src/app/customPipes/product-filter.pipe.ts
@@ -5,7 +5,7 @@ import { Product } from '../layout/models/product';
 export class ProductFilterPipe implements PipeTransform {
   transform(
     itemList: any,
-    searchKeyword: string | Array<{}> | Map<string, Set<string>>
+    searchKeyword: Map<string, Set<string>>
   ) {
     console.log("data for filter***",itemList)
     if (!itemList) return [];
@@ -13,40 +13,64 @@ export class ProductFilterPipe implements PipeTransform {
       return itemList;
     }
     let filteredList = [];
-    if (itemList.length > 0) {
-      let searchKeywords =
-        typeof searchKeyword == 'string'
-          ? searchKeyword.toLowerCase()
-          : (searchKeyword as Map<string, Set<string>>);
-       if (typeof searchKeyword == 'object') {
-        console.log('searchKeyword Map', searchKeyword);
+    if ( itemList) {
+      
         
-        itemList.forEach(item => {
-          let product = item
-          let filterMap = searchKeyword;
-          filterMap.forEach((values,key)=>{
-            let foundkeyValue : Array<string> = [];
-            if(Array.isArray(product[key])){
-              let allKeyValue = product[key] as Array<{}>;
-              allKeyValue.forEach((element:{})=>foundkeyValue.push(element['name']))
-              console.log("isArray",foundkeyValue)
-            }else { 
-              foundkeyValue.push(product[key].name)
+        // itemList.forEach(item => {
+        //   let product = item
+        //   let filterMap = searchKeyword;
+        //   filterMap.forEach((values,key)=>{
+        //     let foundkeyValue : Array<string> = [];
+        //     if(Array.isArray(product[key])){
+        //       let allKeyValue = product[key] as Array<{}>;
+        //       allKeyValue.forEach((element:{})=>foundkeyValue.push(element['name']))
+        //       console.log("isArray",foundkeyValue)
+        //     }else { 
+        //       foundkeyValue.push(product[key].name)
+        //     }
+        //     let itemSearchOn = new Set(foundkeyValue);
+        //     // one filter 'key' has many 'values'
+        //     values.forEach((value:string) => {
+        //       if(itemSearchOn.has(value)){
+        //         filteredList.push(item);
+        //         console.log(`found ${value}`,item)
+        //         return
+        //       }       
+        //     });
+        //   })
+        // });
+      filteredList = itemList.filter((item) => {
+        console.log('--------------item', item);
+        
+        let include = true;
+        searchKeyword.forEach((value, key) => {
+          let any = false;
+          console.log('value: ', value);
+          console.log('key: ', key);
+
+          value.forEach( (searchKey) => {
+            if( Array.isArray(item[key] )){
+              let values = item[key];
+              values.forEach(element => {
+                any ||= (element.name === searchKey ) ?  true : false;
+              });
+            } else if(typeof item[key] === 'object'){
+              any ||= (item[key].name == searchKey ) ?  true : false;
+
+            } else {
+              any ||= (item[key] === searchKey ) ?  true : false;
             }
-            let itemSearchOn = new Set(foundkeyValue);
-            // one filter 'key' has many 'values'
-            values.forEach((value:string) => {
-              if(itemSearchOn.has(value)){
-                filteredList.push(item);
-                console.log(`found ${value}`,item)
-                return
-              }       
-            });
-          })
-        });
-      }
+          });
+          include &&= any ?  true : false;
+          console.log('include value: ', include );
+          console.log('any value: ', any );
+        }); 
+        return include;
+      });
+
+
     }
-    console.log('filtered Dataa', filteredList);
+
     return filteredList;
   }
 }
